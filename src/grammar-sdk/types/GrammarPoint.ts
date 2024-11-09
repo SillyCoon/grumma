9fc4578f36d8f60ff9cc9ad224497bf87cbbad95
SillyCoon <self-councious@proton.me>
2024-11-09 13:04:31 +0900

    add torfl

--- src/grammar-sdk/types/GrammarPoint.ts
+++ src/grammar-sdk/types/GrammarPoint.ts
@@ -1,6 +1,5 @@
-import { Example } from "./Example";
+import type { Example } from "./Example";
 import type { Exercise } from "./Exercise";
-import type { GrammarPointDto, GrammarPointDb } from "./dto";
 
 export interface GrammarPoint {
 	id: string;
@@ -12,49 +11,5 @@ export interface GrammarPoint {
 	}[];
 	order?: number;
 	exercises: Exercise[];
+	torfl: string;
 }
-export const GrammarPoint = (g: GrammarPointDto): GrammarPoint => {
-	return {
-		id: `${g.id.number}`,
-		title: g.title,
-		structure: g.structure,
-		order: g.order,
-		examples: g.exercises.map((e) => ({
-			ru: Example(e.ru),
-			en: Example(e.en),
-		})),
-		exercises: g.exercises.map((e, i) => ({
-			grammarPointId: `${g.id.number}`,
-			ru: e.ru,
-			en: e.en,
-			ruGrammar: extractGrammar(e.ru) ?? "",
-			enGrammar: extractGrammar(e.en) ?? "",
-			draft: e.helper ?? "",
-			order: i,
-		})),
-	};
-};
-
-export const GrammarPointFromDB = (g: GrammarPointDb): GrammarPoint => {
-	return {
-		id: `${g.id}`,
-		title: g.title,
-		structure: g.structure ?? "",
-		order: g.order ?? undefined,
-		examples: g.exercises.map((e) => ({
-			ru: Example(e.ru),
-			en: Example(e.en),
-		})),
-		exercises: g.exercises.map((e, i) => ({
-			grammarPointId: `${g.id}`,
-			ru: e.ru,
-			en: e.en,
-			ruGrammar: extractGrammar(e.ru) ?? "",
-			enGrammar: extractGrammar(e.en) ?? "",
-			draft: e.helper ?? "",
-			order: i,
-		})),
-	};
-};
-
-const extractGrammar = (exercise: string) => exercise.match(/%([^%]+)%/)?.[1];

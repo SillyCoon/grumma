8147237492b6e1e301a9caf59786837788c113b4
SillyCoon <self-councious@proton.me>
2024-11-10 18:23:39 +0900

    migrate explanations to supabase

--- src/grammar-sdk/grammar.ts
+++ src/grammar-sdk/grammar.ts
@@ -1,14 +1,27 @@
+import { createSupabaseClientInstance } from "libs/supabase";
 import { fetchGrammarFromDb, fetchGrammarPointFromDb } from "./db";
 import { fetchGrammarFromApi, fetchGrammarPointFromApi } from "./realtime";
 
-export const fetchGrammarPoint = (id: string) => {
-	return import.meta.env.PUBLIC_REAL_TIME_CONTENT_UPDATE
-		? fetchGrammarPointFromApi(id)
-		: fetchGrammarPointFromDb(id);
+export const fetchGrammarPoint = async (id: string) => {
+	const gp = import.meta.env.PUBLIC_REAL_TIME_CONTENT_UPDATE
+		? await fetchGrammarPointFromApi(id)
+		: await fetchGrammarPointFromDb(id);
+	const explanation = await fetchExplanation(id);
+	return gp ? { ...gp, explanation } : undefined;
 };
 
 export const fetchGrammar = () => {
 	return import.meta.env.PUBLIC_REAL_TIME_CONTENT_UPDATE
 		? fetchGrammarFromApi()
 		: fetchGrammarFromDb();
+};
+
+export const fetchExplanation = async (grammarPointId: string | number) => {
+	const supabase = createSupabaseClientInstance();
+
+	const url = supabase.storage
+		.from("explanations")
+		.getPublicUrl(`SON-${grammarPointId}.html`).data.publicUrl;
+
+	return (await fetch(url)).text();
 };

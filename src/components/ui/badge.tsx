ef2b3d83b39d2fe9dbec9dcfab69ad7706e6f404
SillyCoon <self-councious@proton.me>
2024-11-12 21:30:59 +0900

    add dashboard layout

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -7,12 +7,14 @@ import { cva } from "class-variance-authority";
 import { cn } from "~/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center border max-h-5 px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
         default:
           "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+        light:
+          "border-transparent bg-white text-foregrounds-primary hover:bg-primary/80",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:

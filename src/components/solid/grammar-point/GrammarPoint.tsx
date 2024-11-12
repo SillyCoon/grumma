89a8e6d8eb78564ba6844535f2deeccfb9f19423
SillyCoon <self-councious@proton.me>
2024-11-12 19:54:12 +0900

    add prose

--- src/components/solid/grammar-point/GrammarPoint.tsx
+++ src/components/solid/grammar-point/GrammarPoint.tsx
@@ -1,43 +1,58 @@
 import type { GrammarPoint as GrammarPointType } from "@grammar-sdk";
-import { Card } from "@ui/Card";
 import { Example } from "../example/Example";
 import { ForwardButton } from "../generic/ForwardButton";
 import { Title } from "./Title";
 import { For } from "solid-js";
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
 
 type Props = Omit<GrammarPointType, "exercises"> & {
-	explanation?: string;
-	next?: string;
+  explanation?: string;
+  next?: string;
 };
 
 export const GrammarPoint = (props: Props) => {
-	return (
-		<section class="grid gap-5">
-			<div class="flex justify-between items-center">
-				<Title className="flex-auto" title={props.title} />
-				{props.next && <ForwardButton href={props.next} />}
-			</div>
+  return (
+    <section class="grid gap-5">
+      <div class="flex justify-between items-center">
+        <Title className="flex-auto" title={props.title} />
+        {props.next && <ForwardButton href={props.next} />}
+      </div>
 
-			<Card title="Structure">
-				<section
-					class="whitespace-pre-line [&_b]:text-secondary"
-					innerHTML={props.structure}
-				/>
-			</Card>
-			<Card title="Examples">
-				<section class="grid gap-5">
-					<For each={props.examples}>{(e) => <Example {...e} />}</For>
-				</section>
-			</Card>
+      <Card variant="outlined">
+        <CardHeader>
+          <CardTitle>Structure</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <section
+            class="whitespace-pre-line prose [&_b]:text-secondary"
+            innerHTML={props.structure}
+          />
+        </CardContent>
+      </Card>
+      <Card variant="outlined">
+        <CardHeader>
+          <CardTitle>Examples</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <section class="grid gap-5">
+            <For each={props.examples}>{(e) => <Example {...e} />}</For>
+          </section>
+        </CardContent>
+      </Card>
 
-			<Card title="Explanation">
-				{props.explanation && (
-					<section
-						class="whitespace-pre-line [&_strong]:text-secondary"
-						innerHTML={props.explanation}
-					/>
-				)}
-			</Card>
-		</section>
-	);
+      <Card variant="outlined">
+        <CardHeader>
+          <CardTitle>Explanation</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {props.explanation && (
+            <section
+              class="prose prose-slate [&_strong]:text-secondary  max-w-none "
+              innerHTML={props.explanation}
+            />
+          )}
+        </CardContent>
+      </Card>
+    </section>
+  );
 };

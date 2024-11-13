d442ae4fe9afc14b5523857cbe2cad8e7621cf16
SillyCoon <self-councious@proton.me>
2024-11-14 08:33:36 +0900

    remove daisyui from dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -114,9 +114,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require("daisyui"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };

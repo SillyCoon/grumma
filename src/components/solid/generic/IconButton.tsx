ca59d42396c9f3b1a2248e172c85ad03c9fdbbd7
SillyCoon <self-councious@proton.me>
2024-11-16 10:35:50 +0900

    fix button color

--- src/components/solid/generic/IconButton.tsx
+++ src/components/solid/generic/IconButton.tsx
@@ -1,5 +1,6 @@
 import { children } from "solid-js";
 import type { JSX } from "solid-js";
+import { cn } from "~/lib/utils";
 
 export const IconButton = (props: {
   onClick?: () => void;
@@ -11,7 +12,10 @@ export const IconButton = (props: {
   return (
     <button type="button" onClick={props.onClick} disabled={props.disabled}>
       <div
-        class={`h-[40px] w-[40px] cursor-pointer fill-${props.variant ?? "primary"} hover:text-primary/60`}
+        class={cn(
+          "h-[40px] w-[40px] cursor-pointer hover:text-primary/60",
+          `fill-${props.variant}`,
+        )}
       >
         {c()}
       </div>

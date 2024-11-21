7bc4923db4a6dab97d7d0f4ddc6c4a8cc5caad09
SillyCoon <self-councious@proton.me>
2024-11-21 09:24:03 +0900

    add cram to nav

--- src/components/solid/NavButton.tsx
+++ src/components/solid/NavButton.tsx
@@ -17,7 +17,7 @@ export const NavButton = (props: {
         tabIndex={-1}
         onClick={props.onClick}
         disabled={props.disabled}
-        class="inline-flex w-full gap-1 px-4 py-2 font-bold text-foregrounds-primary text-xl disabled:cursor-help disabled:opacity-50 lg:items-center lg:text-primary-foreground lg:hover:bg-secondary/20 lg:hover:shadow-md"
+        class="inline-flex w-full gap-1 px-2 py-2 font-bold text-foregrounds-primary text-xl disabled:cursor-help disabled:opacity-50 lg:items-center lg:text-primary-foreground lg:hover:bg-secondary/20 lg:hover:shadow-md"
         type="button"
       >
         <span>{props.text}</span>

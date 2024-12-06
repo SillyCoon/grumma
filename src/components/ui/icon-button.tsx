ba62b347c92be50eed1ca2fa8ba8c4fb75a754aa
SillyCoon <self-councious@proton.me>
2024-12-06 09:49:48 +0900

    allow icon-button to accept href

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -3,17 +3,21 @@ import type { JSX } from "solid-js";
 import { children } from "solid-js";
 import { cn } from "~/lib/utils";
 
-const variants = cva("h-[40px] w-[40px] cursor-pointer hover:text-primary/60", {
-  variants: {
-    variant: {
-      primary: "fill-primary",
-      secondary: "fill-secondary",
+const variants = cva(
+  "block h-[40px] w-[40px] cursor-pointer hover:text-primary/60",
+  {
+    variants: {
+      variant: {
+        primary: "fill-primary",
+        secondary: "fill-secondary",
+      },
     },
   },
-});
+);
 
 export const IconButton = (props: {
   onClick?: () => void;
+  href?: string;
   variant?: "primary" | "secondary";
   disabled?: boolean;
   children?: JSX.Element;
@@ -21,15 +25,15 @@ export const IconButton = (props: {
   type?: "button" | "submit" | "reset";
 }) => {
   const c = children(() => props.children);
+  const className = () => cn(variants({ variant: props.variant }), props.class);
   return (
     <button
       type={props.type ?? "button"}
       onClick={props.onClick}
       disabled={props.disabled}
+      class={className()}
     >
-      <div class={cn(variants({ variant: props.variant }), props.class)}>
-        {c()}
-      </div>
+      {props.href ? <a href={props.href}>{c()}</a> : <div>{c()}</div>}
     </button>
   );
 };

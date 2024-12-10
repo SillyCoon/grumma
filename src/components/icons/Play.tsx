e1bbf374d183d1bbda8ab5fa13578b5306160029
SillyCoon <self-councious@proton.me>
2024-12-10 09:27:38 +0900

    move lesson start button to the top on big screen

--- src/components/icons/Play.tsx
+++ src/components/icons/Play.tsx
@@ -1,5 +1,10 @@
-export const Play = ({ title }: { title: string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+export const Play = ({ title, size }: { title: string; size?: number }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 -960 960 960"
+  >
     <title>{title ?? "Play"}</title>
     <path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z" />
   </svg>

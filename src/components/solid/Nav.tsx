15a7c20b465f133894adf673dc4c51e447924ade
SillyCoon <self-councious@proton.me>
2025-02-08 20:58:48 +0900

    fix redirects on signin and signout

--- src/components/solid/Nav.tsx
+++ src/components/solid/Nav.tsx
@@ -1,5 +1,6 @@
 import { actions } from "astro:actions";
-import { NavButton } from "./NavButton";
+import { NavButton, NavButtonClass } from "./NavButton";
+import { navigate } from "astro:transitions/client";
 
 export const Nav = (props: {
   loggedIn?: boolean;
@@ -37,14 +38,21 @@ export const Nav = (props: {
       <NavButton text="Help" link="/help" onClick={props.onClick} />
       <form
         method="post"
-        action={props.loggedIn ? actions.logout : actions.loginWithGoogle}
+        action={!props.loggedIn ? actions.loginWithGoogle : ""}
+        onSubmit={async (e) => {
+          if (props.loggedIn) {
+            e.preventDefault();
+            await actions.logout(new FormData());
+            navigate("/");
+          }
+        }}
       >
         <button
           onClick={props.onClick}
           value="google"
           name="provider"
           type="submit"
-          class="inline-flex w-full gap-1 px-4 py-2 font-bold text-foregrounds-primary text-xl disabled:cursor-help disabled:opacity-50 lg:items-center lg:text-primary-foreground lg:hover:bg-secondary/20 lg:hover:shadow-md"
+          class={NavButtonClass}
         >
           {props.loggedIn ? "Logout" : "Login"}
         </button>

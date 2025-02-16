7a3a632972e48d7c32f97a64fb0613d158e8ba45
SillyCoon <self-councious@proton.me>
2025-02-16 21:26:38 +0900

    fix Session test

--- packages/space-repetition/src/impl/Session.test.ts
+++ packages/space-repetition/src/impl/Session.test.ts
@@ -6,7 +6,6 @@ describe("Session.calculateResult", () => {
   it("should return correct results when all attempts are correct", () => {
     const attempts11 = [
       mockAttempt({ grammarPointId: "1", stage: 1, isCorrect: true }),
-      mockAttempt({ grammarPointId: "1", stage: 1, isCorrect: true }),
     ];
 
     const attempts21 = [
@@ -18,42 +17,78 @@ describe("Session.calculateResult", () => {
     const result = Session.calculateResult(session);
 
     expect(result).toEqual({
-      attempts: {
-        "1-1": attempts11,
-        "2-1": attempts21,
-      },
+      attempts: [...attempts11, ...attempts21],
       correct: 2,
       total: 2,
     });
   });
 
   it("should return correct results when some attempts are incorrect", () => {
+    const latest = new Date("2020-10-10");
+    const before = new Date("2020-10-09");
+    const beforeBefore = new Date("2020-10-08");
+
+    const attempt11 = mockAttempt({
+      grammarPointId: "1",
+      stage: 1,
+      isCorrect: false,
+      answeredAt: before,
+    });
+
+    const attempt21 = mockAttempt({
+      grammarPointId: "2",
+      stage: 1,
+      isCorrect: true,
+    });
+
     const session = mockSession([
-      mockAttempt({ grammarPointId: "1", stage: 1, isCorrect: true }),
-      mockAttempt({ grammarPointId: "1", stage: 1, isCorrect: false }),
-      mockAttempt({ grammarPointId: "2", stage: 1, isCorrect: true }),
+      mockAttempt({
+        grammarPointId: "1",
+        stage: 1,
+        isCorrect: true,
+        answeredAt: latest,
+      }),
+      mockAttempt({
+        grammarPointId: "1",
+        stage: 1,
+        isCorrect: false,
+        answeredAt: beforeBefore,
+      }),
+      attempt11,
+      attempt21,
     ]);
 
     const result = Session.calculateResult(session);
 
     expect(result).toEqual({
-      attempts: session.attempts,
+      attempts: [attempt11, attempt21],
       correct: 1,
       total: 2,
     });
   });
 
   it("should return correct results when all attempts are incorrect", () => {
+    const attempt11 = mockAttempt({
+      grammarPointId: "1",
+      stage: 1,
+      isCorrect: false,
+    });
+    const attempt21 = mockAttempt({
+      grammarPointId: "2",
+      stage: 1,
+      isCorrect: false,
+    });
+
     const session = mockSession([
-      mockAttempt({ grammarPointId: "1", stage: 1, isCorrect: false }),
       mockAttempt({ grammarPointId: "1", stage: 1, isCorrect: true }),
-      mockAttempt({ grammarPointId: "2", stage: 1, isCorrect: false }),
+      attempt11,
+      attempt21,
     ]);
 
     const result = Session.calculateResult(session);
 
     expect(result).toEqual({
-      attempts: session.attempts,
+      attempts: [attempt11, attempt21],
       correct: 0,
       total: 2,
     });

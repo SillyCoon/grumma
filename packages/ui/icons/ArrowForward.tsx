e6c4d1ee73743f0b8402e6177af824377f28204e
aleksei-commune <ivanov.aleksei@commune.co.jp>
2025-02-17 22:16:21 +0900

    improve responsive design

--- packages/ui/icons/ArrowForward.tsx
+++ packages/ui/icons/ArrowForward.tsx
@@ -1,6 +1,10 @@
-export const ArrowForward = (props: { title?: string }) => {
+export const ArrowForward = (props: { class?: string; title?: string }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class={props.class}
+      viewBox="0 -960 960 960"
+    >
       <title>{props.title ?? "Forward"}</title>
       <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
     </svg>

6cceb345c2ccd1d7ba9cc9312f558c5ee378173e
SillyCoon <self-councious@proton.me>
2025-03-04 14:24:31 +0900

    add session result for practice

--- src/components/solid/Exercise/Exercises.tsx
+++ src/components/solid/Exercise/Exercises.tsx
@@ -1,41 +1,46 @@
-import { saveAttempt } from "@services/sr";
+import { clearPracticeSession, saveAttempt } from "@services/sr";
 import { navigate } from "astro:transitions/client";
 import type { Exercise as ExerciseType } from "grammar-sdk";
-import { Match, Switch, createEffect, createSignal } from "solid-js";
+import { Match, Switch, createEffect, createSignal, onMount } from "solid-js";
 import type { Stage } from "space-repetition";
 import { Spinner } from "ui/Spinner";
 import { simpleShuffle } from "utils/array";
 import { v4 as uuidv4 } from "uuid";
 import { Exercise } from "./Exercise";
 
-export const Exercises = (props: { exercises: ExerciseType[] }) => {
-  const sessionId = uuidv4();
-
+export const Exercises = (props: {
+  exercises: ExerciseType[];
+  type: "sr" | "practice";
+}) => {
   const [exercisesLeft, setExercisesLeft] = createSignal<ExerciseType[]>(
     simpleShuffle(props.exercises),
   );
 
   const exercise = () => exercisesLeft().at(0);
 
-  const isSr = window.location.pathname.includes("sr");
+  const isSr = () => props.type === "sr";
+
+  const sessionId = isSr() ? uuidv4() : "practice";
+
+  onMount(() => {
+    !isSr() && clearPracticeSession();
+  });
 
   const handleNext = (
     completed: ExerciseType,
     result: { answer: string; correct: boolean },
   ) => {
-    if (isSr) {
-      const e = exercise();
-      if (e) {
-        saveAttempt({
-          answer: result.answer,
-          isCorrect: result.correct,
-          answeredAt: new Date(),
-          grammarPointId: e.grammarPointId,
-          reviewSessionId: sessionId,
-          // TODO: this will work only if amount of exercises is the same as the amount of stages
-          stage: (exercise()?.order ?? 1) as Stage,
-        });
-      }
+    const e = exercise();
+    if (e) {
+      saveAttempt({
+        answer: result.answer,
+        isCorrect: result.correct,
+        answeredAt: new Date(),
+        grammarPointId: e.grammarPointId,
+        reviewSessionId: sessionId,
+        // TODO: this will work only if amount of exercises is the same as the amount of stages
+        stage: (exercise()?.order ?? 1) as Stage,
+      });
     }
 
     setExercisesLeft((left) =>
@@ -47,8 +52,7 @@ export const Exercises = (props: { exercises: ExerciseType[] }) => {
 
   createEffect(() => {
     if (!exercise()) {
-      if (isSr) navigate(`/sr/review/${sessionId}/result`);
-      else navigate("/dashboard");
+      navigate(`/sr/review/${sessionId}/result`);
     }
   });
 
--- src/components/solid/Exercise/SessionResult.tsx
+++ src/components/solid/Exercise/SessionResult.tsx
@@ -1,59 +1,69 @@
+import { getPracticeSessionResults } from "@services/practice";
+import type { GrammarPoint } from "grammar-sdk";
 import { Example as ExampleImpl } from "grammar-sdk/example";
+import { For, Show } from "solid-js";
 import type { SessionResult as SessionResultType } from "space-repetition/session";
 import { Card, CardContent, CardHeader } from "ui/card";
 import { Example } from "../example/Example";
 
 export const SessionResult = (props: {
-  sessionResult: SessionResultType;
+  sessionResult?: SessionResultType;
   grammar: GrammarPoint[];
 }) => {
+  const sessionResult = () =>
+    props.sessionResult ?? getPracticeSessionResults();
+
   const answers = () =>
-    props.sessionResult.attempts.map((attempt) => {
+    sessionResult()?.attempts?.map?.((attempt) => {
       const gp = props.grammar.find((gp) => gp.id === attempt.grammarPointId);
       const example = gp?.examples.find((e) => e.order === attempt.stage);
       return {
         ...example,
         ru: example?.ru
-          ? ExampleType.replaceAnswer(example.ru, attempt.answer)
+          ? ExampleImpl.replaceAnswer(example.ru, attempt.answer)
           : undefined,
         isCorrect: attempt.isCorrect,
         grammarPointId: attempt.grammarPointId,
       };
     }) as {
-      ru: ExampleType;
-      en: ExampleType;
+      ru: ExampleImpl;
+      en: ExampleImpl;
       isCorrect: boolean;
       grammarPointId: string;
     }[];
 
-  const backTo = () => `/sr/review/${props.sessionResult.sessionId}/result`;
+  const backTo = () => `/sr/review/${sessionResult()?.sessionId}/result`;
 
   const grammarHref = (gpId: string) => `/grammar/${gpId}?backTo=${backTo()}`;
 
   return (
-    <div>
-      <h1 class="mb-5 text-center text-3xl">Молодец!</h1>
+    <Show when={sessionResult()}>
+      {(sessionResult) => (
+        <div>
+          <h1 class="mb-5 text-center text-3xl">Молодец!</h1>
 
-      <div class="grid grid-cols-1 gap-5 md:grid-cols-4">
-        <Accuracy
-          total={props.sessionResult.total}
-          correct={props.sessionResult.correct}
-        />
-        <div class="flex flex-col gap-3 md:col-span-3">
-          <For each={answers()}>
-            {(answer) => (
-              <Example
-                variant={answer.isCorrect ? "correct" : "wrong"}
-                ru={answer.ru}
-                en={answer.en}
-                alwaysShow
-                grammarHref={grammarHref(answer.grammarPointId)}
-              />
-            )}
-          </For>
+          <div class="grid grid-cols-1 gap-5 md:grid-cols-4">
+            <Accuracy
+              total={sessionResult().total}
+              correct={sessionResult().correct}
+            />
+            <div class="flex flex-col gap-3 md:col-span-3">
+              <For each={answers()}>
+                {(answer) => (
+                  <Example
+                    variant={answer.isCorrect ? "correct" : "wrong"}
+                    ru={answer.ru}
+                    en={answer.en}
+                    alwaysShow
+                    grammarHref={grammarHref(answer.grammarPointId)}
+                  />
+                )}
+              </For>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Show>
   );
 };
 

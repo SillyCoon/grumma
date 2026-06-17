7b05fbe0ec866118dbcf5d7848f6808fd4951644
SillyCoon <self-councious@proton.me>
2026-06-17 17:57:48 +0900

    add simple cache utility

---
+++ libs/cache/index.ts
@@ -0,0 +1,55 @@
+import { eq } from "drizzle-orm";
+import { cache } from "libs/db/schema";
+import { db } from "libs/db";
+import crypto from "node:crypto";
+
+const set = async (key: string, value: object, ttlSeconds?: number) => {
+  const expiresAt = ttlSeconds
+    ? new Date(Date.now() + ttlSeconds * 1000)
+    : null;
+
+  await db.insert(cache).values({
+    key,
+    value,
+    expiresAt,
+    insertedAt: new Date(),
+  });
+};
+
+const setByHash = async (value: object, ttlSeconds?: number) => {
+  const hash = crypto
+    .createHash("sha256")
+    .update(JSON.stringify(value))
+    .digest("hex");
+
+  await set(hash, value, ttlSeconds);
+  return hash;
+};
+
+const get = async (key: string) => {
+  const data = await db
+    .select()
+    .from(cache)
+    .where(eq(cache.key, key))
+    .execute();
+
+  if (data.length === 0) {
+    return null;
+  }
+
+  const entry = data[0];
+
+  if (entry.expiresAt && new Date(entry.expiresAt) < new Date()) {
+    // Entry has expired, delete it
+    await db.delete(cache).where(eq(cache.key, key)).execute();
+    return null;
+  }
+
+  return entry.value;
+};
+
+export const Cache = {
+  set,
+  get,
+  setByHash,
+};

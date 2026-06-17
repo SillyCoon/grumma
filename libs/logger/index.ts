c04559ea2a9abe94c32c6833fa8a4be3e88cfb65
SillyCoon <self-councious@proton.me>
2026-06-18 08:47:04 +0900

    init logger

---
+++ libs/logger/index.ts
@@ -0,0 +1,17 @@
+import pino from "pino";
+import { createGcpLoggingPinoConfig } from "@google-cloud/pino-logging-gcp-config";
+
+const logger = pino.pino(
+  createGcpLoggingPinoConfig(
+    {
+      serviceContext: {
+        service: "grumma",
+      },
+    },
+    {
+      level: "info",
+    },
+  ),
+);
+
+export default logger;

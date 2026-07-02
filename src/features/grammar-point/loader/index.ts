6f7018256ce34b5a638b79a3cb6fc8c9d45906af
SillyCoon <self-councious@proton.me>
2026-07-02 19:12:55 +0900

    add better cache key

--- src/features/grammar-point/loader/index.ts
+++ src/features/grammar-point/loader/index.ts
@@ -31,9 +31,11 @@ export function grammarPointsLoader(): LiveLoader<
     loadCollection: async ({ filter }) => {
       try {
         if (filter && "ids" in filter) {
-          return grammarPointsToEntries(
+          return {...grammarPointsToEntries(
             await fetchGrammarPoints(filter.ids, filter.context),
-          );
+          ), cacheHint: {
+            tags: [`grammar-points-${filter.context.user.role}-${filter.ids.join(",")}`],
+          }};
         }
 
         const context = filter?.context ?? { user: { role: "guest" } };

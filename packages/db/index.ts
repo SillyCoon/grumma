6ec1dd10410333089c8418fd2424ceb45e965216
SillyCoon <self-councious@proton.me>
2026-08-11 18:51:13 +0900

    fallback to empty string in connection

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -5,10 +5,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 import * as tmpSchema from "./schema-tmp";
 
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
-}
+const connectionString = process.env.DATABASE_URL ?? "";
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false });

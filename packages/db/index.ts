9ae72113d03d7b9cc400e7e8cda6cb069dc90db1
SillyCoon <self-councious@proton.me>
2026-08-25 08:46:38 +0900

    use hyperdrive connection string for default db client

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -10,14 +10,15 @@ const connectionString = process.env.DATABASE_URL ?? "";
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client, { schema: { ...schema, ...tmpSchema } });
-export const defaultDb = () =>
-  drizzle(client, { schema: { ...schema, ...tmpSchema } });
-
+export const defaultDb = () => {
+  // @ts-expect-error: env is defined in wrangler.toml
+  const client = postgres(env.HYPERDRIVE.connectionString, { prepare: false });
+  return drizzle(client, { schema: { ...schema, ...tmpSchema } });
+};
 export type DbClient = typeof db;
 
 export const makeDb = (connectionString: string) =>
   drizzle(postgres(connectionString, { prepare: false }), {
     schema: { ...schema, ...tmpSchema },
   });
-
 export type Transaction = Parameters<Parameters<DbClient["transaction"]>[0]>[0];
